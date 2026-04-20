b253636fc87c5000c8f2331c594ec5e4ff4e072c
1215-tech <ivan.tulskuy@gmail.com>
2026-04-20 23:41:08 +0200

    revert: restore original formatting configuration

--- quartz/plugins/transformers/formatting_improvement_html.ts
+++ quartz/plugins/transformers/formatting_improvement_html.ts
@@ -699,7 +699,7 @@ export const improveFormatting = (options: Options = {}): Transformer<Root, Root
         const eltsToTransform = collectTransformableElements(node as Element, toSkip)
         eltsToTransform.forEach((elt) => {
           for (const transform of checkedTextTransformers) {
-            transformElement(elt, transform, toSkip, markerChar, false) // Disabled invariance check for custom content
+            transformElement(elt, transform, toSkip, markerChar, true)
           }
 
           for (const transform of activeUncheckedTransformers) {
@@ -712,7 +712,7 @@ export const improveFormatting = (options: Options = {}): Transformer<Root, Root
             return !hasClass(n, "fraction") && n?.tagName !== "a"
           }
           if (slashPredicate(elt)) {
-            transformElement(elt, spacesAroundSlashes, toSkip, markerChar, false) // Disabled invariance check for custom content
+            transformElement(elt, spacesAroundSlashes, toSkip, markerChar, true)
           }
         })
       }
