b86d56a348a0e353ba76738ba8d52b660e96c89e
1215-tech <ivan.tulskuy@gmail.com>
2026-04-24 00:32:45 +0200

    manual3

--- quartz/plugins/filters/draft.ts
+++ quartz/plugins/filters/draft.ts
@@ -4,7 +4,8 @@ export const RemoveDrafts: QuartzFilterPlugin = () => ({
   name: "RemoveDrafts",
   shouldPublish(_ctx, [, vfile]) {
     const filePath = vfile.data.filePath ?? vfile.path ?? ""
-    if (vfile.data.frontmatter?.publish === false) {
+    const publish = vfile.data.frontmatter?.publish
+    if (publish === false || publish === "false") {
       return false
     }
     return !filePath.includes("drafts/") || filePath.includes("templates/")
